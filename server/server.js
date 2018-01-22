a9c12405ce0f3079f033edd4f276518975d7f8d2
Prakash <l352618@KPAdmins-MacBook-Pro.local>
2018-01-21 21:04:49 -0800

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
-
-const request = require('request');
-const express = require('express');
-
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+console.log('New user connected');
+
+socket.on('connect', () => {
+  console.log('Connnected to Server');
+});
+
+socket.on('disconnect', () => {
+  console.log('User was disconnected from server');
+});
+
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
 
